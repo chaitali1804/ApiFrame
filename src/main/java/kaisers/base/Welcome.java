97b52626ec892553f89bfebf7e4fb29755f7ee96
chaitali1804 <chaitali.mahalankar@gmail.com>
2022-02-23 16:28:19 +0530

    this is welcome commit

---
+++ src/main/java/kaisers/base/Welcome.java
@@ -0,0 +1,9 @@
+package kaisers.base;
+
+public class Welcome {
+
+	private void m1() {
+System.out.println("welcome");
+System.out.println("welcome");System.out.println("welcome");System.out.println("welcome");
+	}
+}
